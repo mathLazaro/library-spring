ff6eed906d2ffd2d7e4329c8fd046eeca4f7c25c
Matheus Lazaro <matheus.lazaro2002@gmail.com>
2025-02-06 16:50:25 -0300

    docker image build

--- src/main/java/com/example/demoJpa/configuration/AuditingConfiguration.java
+++ src/main/java/com/example/demoJpa/configuration/AuditingConfiguration.java
@@ -1,8 +1,7 @@
 package com.example.demoJpa.configuration;
 
-import com.example.demoJpa.domain.Users;
-import com.example.demoJpa.repository.UsersRepository;
 import com.example.demoJpa.auditing.SpringSecurityAuditorAware;
+import com.example.demoJpa.domain.Users;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
